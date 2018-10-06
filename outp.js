38b38d7c0692debbaaca4e0f0f92511f4c03bda1
Bobscript <painty@users.noreply.github.com>
2018-10-06 15:47:31 +0800

    specify img protocol when preview
    location.protocol is "chrome-extension:" on preview page. To manually specify the img src start with "//" to "http://".

--- outp.js
+++ outp.js
@@ -1,17 +1,22 @@
+/* global chrome */
+
 function qr(sel){
     return document.querySelector(sel);
 }
 qr('#newwin').addEventListener('click', function(){
+    var regNoProtocol=/(['"]|\(['"]?)\/\//g;
+    var strAddProtocol='$1http://';
+    var html=qr('#outp1').value.replace(regNoProtocol,strAddProtocol);
+    var css=qr('#outp2').value.replace(regNoProtocol,strAddProtocol);
+
     var w=window.open();
     setTimeout(() => {
-        w.document.write('<!DOCTYPE html>'+document.getElementById('outp1').value);
-
-        var css=document.getElementById('outp2').value;
+        w.document.write('<!DOCTYPE html>'+html);
         var style = w.document.createElement('style');
         style.type = 'text/css';
         style.appendChild(w.document.createTextNode(css))
         w.document.head.appendChild(style);
-    }, 100);
+    }, 200);
 });
 
 qr('#issuebtn').addEventListener('click', function(){
