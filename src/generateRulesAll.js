53a42d53dda7d082e78ebc8742266f0ffe2e9d6b
Bobscript <painty@users.noreply.github.com>
2019-01-15 11:47:05 +0800

    no innerHTML but rules in <style>

--- src/generateRulesAll.js
+++ src/generateRulesAll.js
@@ -28,10 +28,20 @@ function generateRulesAll(doc, externalCssCache) {
           })
         } else {
           // style tag
+          let html=doc.styleSheets[x].ownerNode.innerHTML;
+          if(html===''){
+            // style may be in style-tag's cssRules but not show in innerHTML
+            for (let index = 0; index < doc.styleSheets[x].cssRules.length; index++) {
+              const rule = doc.styleSheets[x].cssRules[index];
+              html+=rule.cssText;
+            }
+          }
           // convert urls in style tag to abs
-          let html = doc.styleSheets[x].ownerNode.innerHTML.replace(/url\((['"]?)(.*?)\1\)/g, function (a, p1, p2) {
+          html = html.replace(/url\((['"]?)(.*?)\1\)/g, function (a, p1, p2) {
             return 'url(' + convUrlToAbs(doc.location.href, p2) + ')';
           });
+          // the next operation is asynchronous
+          // store the current x value
           let _x = x;
           convTextToRules(html, doc.location.href).then(function (cssNodeArr) {
             cssNodeArr.media = doc.styleSheets[_x].media;
