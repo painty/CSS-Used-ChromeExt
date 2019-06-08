41d8eb09f25fbaf4cc89e4fd0d1e3a1915016ccc
Bobscript <painty@users.noreply.github.com>
2019-06-09 00:21:39 +0800

    from browserify to parcel

--- test/expected.js
+++ test/expected.js
@@ -1,15 +1,16 @@
-var expected=`/*! CSS Used from: http://127.0.0.1:8080/test/index.css */
-/*! @import http://127.0.0.1:8080/test/common.cssscreen, projection */
+function expected(port){
+  return `/*! CSS Used from: http://localhost:${port}/index.css */
+/*! @import http://localhost:${port}/common.cssscreen, projection */
 @media screen, projection{
 body{background:#ccc;}
 }
 /*! end @import */
-/*! @import http://127.0.0.1:8080/test/landscape.cssscreen and (orientation:portrait) */
+/*! @import http://localhost:${port}/landscape.cssscreen and (orientation:portrait) */
 @media screen and (orientation:portrait){
 body{background:#aaa;}
 }
 /*! end @import */
-/*! @import http://127.0.0.1:8080/test/skin.css */
+/*! @import http://localhost:${port}/skin.css */
 .S_spetxt{color:#fa7d3c;}
 body{background-color:#b4d66b;background-image:none;}
 body{color:#333;text-decoration:none;}
@@ -17,7 +18,7 @@ a{color:#76a513;}
 /*! end @import */
 :AFTER{content:'after';background:#38c;}
 html{height:100%;}
-A{background:url(http://127.0.0.1:8080/test/logo.jpg);background-size:100%;animation:teatnamekey;}
+A{background:url(http://localhost:${port}/logo.jpg);background-size:100%;animation:teatnamekey;}
 @media screen and (min-width:500px){
 a{width:16.666%;}
 }
@@ -30,7 +31,7 @@ body{font-family:"WBswficon";}
 @media print{
 i{font-size:10pt;}
 }
-/*! CSS Used from: http://127.0.0.1:8080/test/font.css */
+/*! CSS Used from: http://localhost:${port}/font.css */
 i{font-family:'Cutive Mono';}
 @media only screen and (max-width:480px){
 b{font-size:20px;}
@@ -45,3 +46,6 @@ i{color:#789;font-family:WBswficon;}
 @keyframes teatnamekey{0%{width:10px;}100%{height:10px;}}
 /*! CSS Used fontfaces */
 @font-face{font-family:'WBswficon';src:url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.eot?id=141111232016");src:url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.eot?id=141111232016#iefix")  format('embedded-opentype'), url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.svg?id=141111232016")  format('svg'), url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.woff?id=141111232016")  format('woff'), url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.ttf?id=141111232016")  format('truetype');src:url("http://img.t.sinajs.cn/t6/style//images/common/font/swbficon.eot?id=141111232016") \\9;font-weight:normal;font-style:normal;}`
+}
+
+export default expected
